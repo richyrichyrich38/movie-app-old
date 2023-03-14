c29d49b17a19183b7ad183856284febaa079a751
richyrichyrich38 <85546279+richyrichyrich38@users.noreply.github.com>
2023-03-14 14:12:22 +0000

    js movie data added and updated css

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// Grab html elements
+// Get the inputs value on enter key press
+// Grab data related related to users search
+// Inject movie items into the dom based on users search
+
+
+let searchInput = document.getElementById('#search');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function init() {
+  onclick.getMovieData(keydown, )
+
+}
+
+init();
+
+/* <div class="movie-item">
+  <h3>Movie Title</h3>
+  <p>lorum hwhh  whuhwuuy  ywyuyhuwu yuyuowyuy uywpuy ywyu </p>
+  <a href="#">View More Details</a>
+</div> */
