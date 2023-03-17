501737ee76d1fa8db9755d861fde66c159bb2ea0
richyrichyrich38 <85546279+richyrichyrich38@users.noreply.github.com>
2023-03-17 13:59:25 +0000

    before adding API for data

--- js/app.js
+++ js/app.js
@@ -5,19 +5,46 @@
 
 
 let searchInput = document.querySelector('.search');
-let itemWrapper = document.querySelector('main')
+let itemWrapper = document.querySelector('main');
+let searchPara = document.querySelector('.search-p');
 
+function displayMatches(matches) {
+  itemWrapper.innerHTML = '';
+  searchPara.innerHTML = '';
 
+  for (match of matches) {
+    itemWrapper.insertAdjacentHTML("beforeend", `
+    <div class="movie-item" style="background-image: 
+    linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${match.image_url})">
+    <h3>${match.title}</h3>
+    <p>${match.description}</p>
+    <a href="${match.imdb_url}" target="_blank">View More Details</a>
+    </div>
+    `)
+
+    console.log(match);
+  }
+}``
 
 
 function getMovieData(event) {
   var keyCode = event.keyCode;
-  var searchText = searchInput.value.trim()
+  var searchText = searchInput.value.trim().toLowerCase()
 
-  if (keyCode === 13) {
-    for(searchText)
+  if (keyCode === 13 && searchText) {
+    var matches = [];
+
+    for (movie of movieData) {
+      if (movie.title.toLowerCase().includes(searchText)) {
+        matches.push(movie)
+        
+      }
+
+
+    }
+    displayMatches(matches)
   };
-  console.log(keyCode);
+  // console.log(keyCode);
 }
 
 
